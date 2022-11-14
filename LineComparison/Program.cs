d2342fa533224c72cabb9987f1116a3c267e082a
dericklopis <stanforever08@gmail.com>
2022-11-14 23:04:56 +0530

    Added: Length of the line

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -10,7 +10,18 @@ namespace LineComparison
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to line comparison");
+            int x1 , y1 ,x2 , y2 ;
+            Console.WriteLine("Enter value of x1:");
+            x1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value of y1:");
+            y1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value of x2:");
+            x2=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value of y2:");
+            y2=Convert.ToInt32(Console.ReadLine());
+
+            double lengthofline = Math.Sqrt(Math.Pow((x2-x1), 2)+Math.Pow((y2-y1), 2));
+            Console.WriteLine("The length of the line is :"+lengthofline);
             Console.ReadLine();
         }
     }
