394177ff302c37bd427cd76f767f3892813c3f06
dericklopis <stanforever08@gmail.com>
2022-11-14 23:23:40 +0530

    Added: Lines are equal or not

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -20,8 +20,17 @@ namespace LineComparison
             Console.WriteLine("Enter value of y2:");
             y2=Convert.ToInt32(Console.ReadLine());
 
-            double lengthofline = Math.Sqrt(Math.Pow((x2-x1), 2)+Math.Pow((y2-y1), 2));
-            Console.WriteLine("The length of the line is :"+lengthofline);
+            int lengthofline1 = Math.Abs(x1-y1);
+            int lengthofline2 = Math.Abs(x2-y2);
+            if (lengthofline1==lengthofline2)
+            {
+                Console.WriteLine("Both line are equal in length");
+            }
+            else
+            {
+                Console.WriteLine("Lines are not equal");
+            }
+            
             Console.ReadLine();
         }
     }
